e724b6dfb184f99059279e35a32a20d279811651
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-02-18 13:14:58 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -61,6 +61,12 @@ $(document).ready(function(){
 		$('.donate_popup').show(); 
 	});	
 
+	$('.counter').counterUp({
+		delay: 10,
+		time: 2000
+	  });
+	  $('.counter').addClass('animated fadeInDownBig');
+	  $('h3').addClass('animated fadeIn');
 	
 
 
